6601299c39df3e469b4d974a0ceea484c48da90f
shubhupareek9 <52706971+shubhupareek9@users.noreply.github.com>
2026-04-20 17:42:17 -0400

    Update ExtentManager.java

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -1,1 +1,24 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    public static ExtentReports getExtent() {
+
+        if (extent == null) {
+
+            ExtentSparkReporter reporter =
+                    new ExtentSparkReporter("extent-report.html");
+
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+
+            extent.setSystemInfo("Project", "Selenium Automation");
+            extent.setSystemInfo("Tester", "You");
+        }
+
+        return extent;
+    }
+}
