6c92238ae46a16b6c4c68e20900a8efb7f5cbc29
shubhupareek9 <52706971+shubhupareek9@users.noreply.github.com>
2026-04-20 09:52:47 -0400

    Update GoogleTest.java

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,1 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
 
+public class GoogleTest {
+
+    @Test
+    public void openGoogle() {
+
+        // Launch Chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        // Open Google
+        driver.get("https://www.google.com");
+
+        // Print page title
+        System.out.println("Page Title: " + driver.getTitle());
+
+        // Close browser
+        driver.quit();
+    }
+}
