3480ebd0db651bf7fd25b8f4df5cf81318fff1ea
shubhupareek9 <52706971+shubhupareek9@users.noreply.github.com>
2026-04-21 11:57:29 -0400

    Update ExtentManager.java

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -7,22 +7,20 @@ public class ExtentManager {
 
     private static ExtentReports extent;
 
-    public static ExtentReports getExtent() {
+    public static synchronized ExtentReports getExtent() {
 
         if (extent == null) {
 
             ExtentSparkReporter spark =
                     new ExtentSparkReporter("target/extent-report.html");
 
-            spark.config().setReportName("Selenium Automation Report");
-            spark.config().setDocumentTitle("Test Execution Results");
+            spark.config().setDocumentTitle("Automation Report");
+            spark.config().setReportName("Selenium Execution Report");
 
             extent = new ExtentReports();
             extent.attachReporter(spark);
 
-            extent.setSystemInfo("Project", "Selenium Extent Demo");
-            extent.setSystemInfo("Environment", "QA");
-            extent.setSystemInfo("Framework", "JUnit 5 + Selenium");
+            extent.setSystemInfo("Framework", "JUnit5 + Selenium");
         }
 
         return extent;
