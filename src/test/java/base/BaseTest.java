d10decde077a95b6d0f524259add6adcd31c7803
shubhupareek9 <52706971+shubhupareek9@users.noreply.github.com>
2026-04-21 15:05:12 -0400

    Create BaseTest.java

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,32 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+import org.testng.annotations.*;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
